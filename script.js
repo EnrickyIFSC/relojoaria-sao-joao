7510636b7c8b478701ae2d852ce20f5ed6a0a14e
Kaynan-Jamelaumn <kaynanrodrigues.nt@gmail.com>
2023-06-20 22:00:13 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let open = document.getElementById("burger-button");
 let menu = document.getElementById("main-menu");
+
 open.addEventListener("click", () => {
   open.style.display = "none";
   menu.classList.toggle("show");
