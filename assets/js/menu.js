d3f5c4553c0e42ccd6a31e74a94296b192a7502a
Enricky Hipólito <enricky.hipolito@gmail.com>
2023-06-23 17:41:26 -0300

    fecha menu ao clicar fora dele

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,22 @@
 const openButton = document.getElementById("burger-button");
 const menu = document.getElementById("main-menu");
-
-openButton.addEventListener("click", () => {
-  openButton.style.display = "none";
-  menu.classList.toggle("show");
-})
-
 const closeButton = document.getElementById("close-button");
-closeButton.addEventListener("click", () => {
+
+function toggleMenu() {
   menu.classList.toggle("show");
-  openButton.style.display = "block";
-})
+  openButton.style.display = menu.classList.contains("show") ? "none" : "block";
+}
+
+openButton.addEventListener("click", toggleMenu);
+
+document.addEventListener("click", (event) => {
+  const isMenuVisible = menu.classList.contains("show");
+
+  if (!isMenuVisible || event.target === openButton) {
+    return;
+  }
+
+  if (!menu.contains(event.target) || event.target === closeButton) {
+    toggleMenu();
+  }
+});
