b8c11f937482f319166eccdf71c4c2b36d65c88b
Enricky Hipólito <enricky.hipolito@gmail.com>
2023-06-23 16:05:53 -0300

    removendo zoom em telas pequenas

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,20 +1,23 @@
 const boxes = document.querySelectorAll(".box-imagem-produto");
+const mediaQuery = window.matchMedia("(min-width: 1000px)");
 
-boxes.forEach(box => {
-  const img = box.getElementsByTagName("img")[0];
-
-  box.addEventListener("mousemove", (event) => {
-    const offset = box.getBoundingClientRect();
-
-    const x = event.clientX - offset.left;
-    const y = event.clientY - offset.top;
-      
-    img.style.transformOrigin = `${x}px ${y}px`;
-    img.style.transform = "scale(2.5)";
+if (mediaQuery.matches) {
+  boxes.forEach(box => {
+    const img = box.getElementsByTagName("img")[0];
+  
+    box.addEventListener("mousemove", (event) => {
+      const offset = box.getBoundingClientRect();
+  
+      const x = event.clientX - offset.left;
+      const y = event.clientY - offset.top;
+        
+      img.style.transformOrigin = `${x}px ${y}px`;
+      img.style.transform = "scale(2.5)";
+    })
+  
+    box.addEventListener("mouseleave", () => {
+      img.style.tranformOrigin = `center center`;
+      img.style.transform = "scale(1)";
+    })
   })
-
-  box.addEventListener("mouseleave", () => {
-    img.style.tranformOrigin = `center center`;
-    img.style.transform = "scale(1)";
-  })
-})
+}
